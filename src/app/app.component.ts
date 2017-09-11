fe52bcbd68a7a2aa4efb227db49a18cd7d24e59b
nirkaufman <nirkaufman@gmail.com>
2017-09-11 16:22:19 +0300

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,13 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+    <app-todolist></app-todolist>
+  `
 })
+
 export class AppComponent {
-  title = 'app';
+
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,19 @@
+import { TodolistComponent } from './todolist/todolist.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent, TodolistComponent],
+  imports     : [BrowserModule],
+  bootstrap   : [AppComponent]
 })
-export class AppModule { }
+
+export class AppModule {
+
+  constructor() {
+
+  }
+
+}
+

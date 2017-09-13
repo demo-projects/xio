d33a772390e25a207048d54fd8627360fa174e38
nirkaufman <nirkaufman@gmail.com>
2017-09-13 12:07:36 +0300

    item binding

--- src/app/todolist/todolist.service.ts
+++ src/app/todolist/todolist.service.ts
@@ -12,6 +12,11 @@ export class TodolistService {
     return this._items;
   }
 
+  public removeItem(item: Item): void {
+    const index = this._items.indexOf(item);
+    this._items.splice(index, 1);
+  }
+
   public addItem(title: string): void {
     this._items.push(new Item(title));
   }

00d68b418e75b0c4b7d89554892c982c44250cba
nirkaufman <nirkaufman@gmail.com>
2017-09-13 18:59:08 +0300

    app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,32 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
+  styles  : [`
+    .line {  text-decoration: underline  }
+    .danger {  color: red  }
+    .success {  color: green  }
+  `],
   template: `
     <app-todolist></app-todolist>
+    <!--<input type="checkbox"-->
+           <!--(change)="flag = !flag">-->
+
+    <!--<h1 class="line" [ngClass]="{ danger: flag }" >Hello Directives!</h1>-->
+
+    <!--<h1 *ngIf="flag">CONTENT OF THE TEMPLATE</h1>-->
+
+    <!--<div ngSwitch="value">-->
+    <!--<div *ngSwitchCase="1">1</div>-->
+    <!--<div *ngSwitchCase="2">2</div>-->
+    <!--<div *ngSwitchDefault>default</div>-->
+    <!--</div>-->
+
   `
 })
 
 export class AppComponent {
-
-
+  flag      = false;
+  className = 'danger';
 
 }
---
+++ src/app/instructions.md
@@ -0,0 +1,13 @@
+No mutable or two-way data bindings!
+
+UI 
+- toggle item should trigger count
+- toggle all and clear all should work
+- inline editing on an item (dynamic css class)
+ - pressing enter updates the value
+ - pressing esc ignore the change
+ 
+Logic 
+- create a storage service to store the list 
+  on localStorage
+ 
